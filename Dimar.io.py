d87cf2d213fa4fa382efd7738f906af16b91bedb
Zentr2350 <93312666+Zentr2350@users.noreply.github.com>
2022-01-03 18:20:29 +0300

    Начата работа с ботами

--- Dimar.io.py
+++ Dimar.io.py
@@ -45,7 +45,7 @@ class Start_window(QMainWindow):
                 self.top = randrange(-4300, high)
                 self.cell_size = 75
 
-            def set_view(self, left, top, cell_size, a, v):
+            def set_view(self, left, top, cell_size, a, v, b):
                 # print((left, top, cell_size, a))
                 # print()
                 # print('----------------------------')
@@ -56,6 +56,9 @@ class Start_window(QMainWindow):
                 for i in range(len(v)):
                     v[i][0] += left
                     v[i][1] += top
+                for i in range(len(b)):
+                    b[i][0] += left
+                    b[i][1] += top
                 self.left += left
                 self.top += top
                 if self.left < -1 * self.cell_size * self.width + width or self.left > width:
@@ -64,15 +67,19 @@ class Start_window(QMainWindow):
                         a[i][0] -= left
                     for i in range(len(v)):
                         v[i][0] -= left
+                    for i in range(len(b)):
+                        b[i][0] -= left
                 if self.top < -1 * self.cell_size * self.width + high or self.top > high:
                     self.top -= top
                     for i in range(len(a)):
                         a[i][1] -= top
                     for i in range(len(v)):
                         v[i][1] -= top
-                return a, v
+                    for i in range(len(b)):
+                        b[i][1] -= top
+                return a, v, b
 
-            def set_view_2(self, k, s, a):
+            def set_view_2(self, k, s, a, v, b):
                 left = self.left - width
                 top = self.top - high
                 left *= k
@@ -84,7 +91,13 @@ class Start_window(QMainWindow):
                 for i in range(len(a)):
                     a[i][0] = (k * (a[i][0] - width)) + width
                     a[i][1] = (k * (a[i][1] - high)) + high
-                return a
+                for i in range(len(v)):
+                    v[i][0] = (k * (v[i][0] - width)) + width
+                    v[i][1] = (k * (v[i][1] - high)) + high
+                for i in range(len(b)):
+                    b[i][0] = (k * (b[i][0] - width)) + width
+                    b[i][1] = (k * (b[i][1] - high)) + high
+                return a, v, b
 
             def render(self, screen):
                 for y in range(self.height):
@@ -100,7 +113,12 @@ class Start_window(QMainWindow):
         def pointss(s, r):
             return [[randrange(board.move()[0], s * 100 + board.move()[0]),  # создание еды
                      randrange(board.move()[1], s * 100 + board.move()[1]),
-                     (randrange(0, 255), randrange(0, 255), randrange(0, 255)), False, r] for _ in range(3000)]
+                     (randrange(0, 255), randrange(0, 255), randrange(0, 255)), r] for _ in range(3000)]
+
+        def bot(s):
+            return [[randrange(board.move()[0], s * 100 + board.move()[0]),  # создание еды
+                     randrange(board.move()[1], s * 100 + board.move()[1]),
+                     (randrange(0, 255), randrange(0, 255), randrange(0, 255)), randrange(20, 350)] for _ in range(20)]
 
         def delenie(x, y, z):
             return [x, y, z]
@@ -143,7 +161,7 @@ class Start_window(QMainWindow):
             points = pointss(size, r_points)
             del_points = []
             kf = 0
-            kff = 15
+            kff = 20
             flag = False
             draw = 1
             r_v = {}
@@ -152,7 +170,10 @@ class Start_window(QMainWindow):
             virus = []
             image1 = pygame.transform.scale(Virus.load_image("virus.png"), (150, 150))
             del_virus = []
-
+            bots = bot(size)
+            del_bots = []
+            kf1 = 0
+            kff1 = 15
             while running:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
@@ -164,9 +185,9 @@ class Start_window(QMainWindow):
                             b = True
                             koord = delenie(x, y, z)
                             r = sqrt((pi * (r ** 2) - pi * (20 ** 2)) / pi)
-                            w.append([koord, kf, kff, True])
+                            w.append([koord, kf1, kff1, True])
                         elif event.key == pygame.K_RSHIFT or event.key == pygame.K_LSHIFT:
-                            if r >= 40:
+                            if r >= 40 and not flag:
                                 koord = delenie(x, y, z)
                                 flag = True
                                 r /= 2
@@ -186,7 +207,7 @@ class Start_window(QMainWindow):
                                    (randrange(0, 255), randrange(0, 255), randrange(0, 255)), r_points])
                 if (x > 50 or x < -50) or (y > 50 or y < -50):
                     move = False
-                    points, virus = board.set_view(0 - (x / z) * v, 0 - (y / z) * v, size, points, virus)
+                    points, virus, bots = board.set_view(0 - (x / z) * v, 0 - (y / z) * v, size, points, virus, bots)
                     width1 -= (x / z) * v
                     high1 -= (y / z) * v
 
@@ -203,20 +224,37 @@ class Start_window(QMainWindow):
                 for i in range(len(points)):
                     if (width - r < points[i][0] < width + r and high - r < points[i][1] < high + r) and r >= points[i][-1]:
                         del_points.append(points[i])
+                for i in range(len(bots)):
+                    if (width - r < bots[i][0] < width + r and high - r < bots[i][1] < high + r) and abs(width - bots[i][0]) < 0.5 * r and abs(high - bots[i][1]) < 0.5 * r and r > bots[i][-1] * 1.05:
+                        del_bots.append(bots[i])
+
+                    elif r * 1.05 < bots[i][-1] and bots[i][0] - bots[i][-1] < width < bots[i][0] + bots[i][-1] and bots[i][1] - bots[i][-1] < high < bots[i][1] + bots[i][-1] and abs(bots[i][0] - width) < 0.5 * bots[i][-1] and abs(bots[i][1] - high) < 0.5 * bots[i][-1]:
+                        running = False
 
                 for i in del_points:
                     if r >= 150:
-                        size *= (pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))
-                        r_points *= size / (size / ((pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))))
-                        k = size / (size / ((pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))))
-                        points = board.set_view_2(k, size, points)
-                        v *= (pi * (r ** 2)) / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))
+                        if size > 40:
+                            size *= (pi * (r ** 2)) / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))
+                            r_points *= size / (
+                                        size / ((pi * (r ** 2)) / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))))
+                            k = size / (size / ((pi * (r ** 2)) / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))))
+
+                            v *= (pi * (r ** 2)) / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))
+                        else:
+                            size *= 0.99999
+                            r_points *= 0.99999
+                            k = 0.99999
+                            v *= 0.99999
+                        points, virus, bots = board.set_view_2(k, size, points, virus, bots)
+
+                        for h in range(len(bots)):
+                            bots[h][-1] *= k
                         for g in range(len(points)):
                             if 0 < points[g][0] < width * 2 and 0 < points[g][1] < high * 2:
-                                pygame.draw.circle(screen, points[g][2], (points[g][0], points[g][1]), 10)
+                                pygame.draw.circle(screen, points[g][2], (points[g][0], points[g][1]), r_points)
                     elif r < 150:
                         r = sqrt(((pi * (r ** 2)) + (pi * (i[-1] ** 2))) / pi)
-                        v *= 0.995
+                        v *= 0.999
 
                     try:
                         del points[points.index(i)]
@@ -226,23 +264,40 @@ class Start_window(QMainWindow):
 
                 for i in del_virus:
                     if r >= 150:
-                        size *= (pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))
+                        size *= (pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2))) * 1.1
                         r_points *= size / (size / ((pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))))
                         k = size / (size / ((pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))))
-                        points = board.set_view_2(k, size, points)
+                        points, virus, bots = board.set_view_2(k, size, points, virus, bots)
+                        for h in range(len(bots)):
+                            bots[h][-1] *= size / (size / ((pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))))
                         v *= (pi * (r ** 2)) / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))
-                        for g in range(len(points)):
-                            if 0 < points[g][0] < width * 2 and 0 < points[g][1] < high * 2:
-                                pygame.draw.circle(screen, points[g][2], (points[g][0], points[g][1]), 10)
                     elif r < 150:
-                        r = sqrt(((pi * (r ** 2)) + (pi * (i[-1] ** 2))) / pi)
-                        v *= 0.995
+
+                        v *= 0.999
 
                     try:
                         del virus[virus.index(i)]
                     except Exception as e:
                         pass
                 del_virus = []
+                for i in del_bots:
+                    if r >= 150:
+                        size *= (pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2))) * 1.1
+                        r_points *= size / (size / ((pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))))
+                        k = size / (size / ((pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))))
+                        points, virus, bots = board.set_view_2(k, size, points, virus, bots)
+                        v *= (pi * (r ** 2)) / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))
+                        for h in range(len(bots)):
+                            bots[h][-1] *= size / (size / ((pi * (r ** 2)) * 1.001 / ((pi * (r ** 2)) + (pi * (i[-1] ** 2)))))
+                    elif r < 150:
+                        v *= 0.999
+                        r = sqrt(((pi * (r ** 2)) + (pi * (i[-1] ** 2))) / pi)
+
+                    try:
+                        del bots[bots.index(i)]
+                    except Exception as e:
+                        pass
+                del_bots = []
                 screen.fill((235, 235, 235))
                 board.render(screen)
 
@@ -283,32 +338,58 @@ class Start_window(QMainWindow):
                             b = False
 
                 if flag:
-                    if kf < 300 and kff > -15.5:  # 0
+                    r2 = r1
+                    if kf < 700 and kff > -20.5:  # 0
+
                         for i in range(len(points)):
-                            if (koord[0] / koord[2]) * kf + width - r1 < points[i][0] < (
-                                    koord[0] / koord[2]) * kf + width + r1 and koord[1] / koord[2] * kf + high - r1 < \
-                                    points[i][1] < koord[1] / koord[2] * kf + high + r1:
+                            if (koord[0] / koord[2]) * kf + width - r2 < points[i][0] < (
+                                    koord[0] / koord[2]) * kf + width + r2 and koord[1] / koord[2] * kf + high - r2 < \
+                                    points[i][1] < koord[1] / koord[2] * kf + high + r2:
                                 del_points.append(points[i])
                         for i in del_points:
-                            r2 = r1
-                            r1 += (sqrt(((pi * (r1 ** 2)) + (pi * (i[-1] ** 2))) / pi) - r2) / 2
+
+                            r2 += (sqrt(((pi * (r2 ** 2)) + (pi * (i[-1] ** 2))) / pi) - r2) / 2
                             try:
                                 del points[points.index(i)]
                             except:
                                 pass
+
+                        for i in range(len(virus)):
+                            if (koord[0] / koord[2]) * kf + width - r2 < virus[i][0] < (
+                                    koord[0] / koord[2]) * kf + width + r2 and koord[1] / koord[2] * kf + high - r2 < \
+                                    virus[i][1] < koord[1] / koord[2] * kf + high + r2:
+                                del_virus.append(virus[i])
+                        for i in del_virus:
+                            r2 += (sqrt(((pi * (r2 ** 2)) + (pi * (i[-1] ** 2))) / pi) - r2) / 2
+                            try:
+                                del virus[virus.index(i)]
+                            except:
+                                pass
+                        for i in range(len(bots)):
+                            if (koord[0] / koord[2]) * kf + width - r2 < bots[i][0] < (
+                                    koord[0] / koord[2]) * kf + width + r2 and koord[1] / koord[2] * kf + high - r2 < \
+                                    bots[i][1] < koord[1] / koord[2] * kf + high + r2 and r2 > bots[i][-1] * 1.05 and \
+                                    abs((koord[0] / koord[2]) * kf + width - bots[i][0]) < 0.5 * r2 and abs(koord[1] / koord[2] * kf + high - bots[i][1]) < 0.5 * r2:
+                                del_bots.append(bots[i])
+                        for i in del_bots:
+                            r2 += (sqrt(((pi * (r2 ** 2)) + (pi * (i[-1] ** 2))) / pi) - r2) / 4
+                            try:
+                                del bots[bots.index(i)]
+                            except:
+                                pass
                         pygame.draw.circle(screen, (200, 0, 0),
                                            ((koord[0] / koord[2]) * kf + width, koord[1] / koord[2] * kf + high),
-                                           r1 + 5)
+                                           r2 + 5)
                         pygame.draw.circle(screen, (255, 0, 0),
-                                           ((koord[0] / koord[2]) * kf + width, koord[1] / koord[2] * kf + high), r1)
+                                           ((koord[0] / koord[2]) * kf + width, koord[1] / koord[2] * kf + high), r2)
                         kf += kff
                         kff -= 0.5
                     else:
 
                         flag = False
                         kf = 0
-                        kff = 15
-                        r += r1
+                        kff = 20
+                        r += r2
 
                 if r >= 70:
                     eat_virus = True
@@ -318,9 +399,14 @@ class Start_window(QMainWindow):
                 if viruss:
                     virus.append([1100, 100, (0, 0, 0), 70])
                     viruss = False
-
+                for u in range(len(bots)):
+                    if 0 < bots[u][0] + bots[u][-1] and bots[u][0] - bots[u][-1] < width * 2 and 0 < bots[u][1] + bots[u][-1] and bots[u][1] - bots[u][-1] < high * 2 and bots[u][-1] < r:
+                        pygame.draw.circle(screen, bots[u][2], (bots[u][0], bots[u][1]), bots[u][-1])
                 pygame.draw.circle(screen, (200, 0, 0), (width, high), r + 5)
                 pygame.draw.circle(screen, (255, 0, 0), (width, high), r)
+                for g in range(len(bots)):
+                    if 0 < bots[g][0] + bots[g][-1] and bots[g][0] - bots[g][-1] < width * 2 and 0 < bots[g][1] + bots[g][-1] and bots[g][1] - bots[g][-1] < high * 2 and bots[g][-1] > r:
+                        pygame.draw.circle(screen, bots[g][2], (bots[g][0], bots[g][1]), bots[g][-1])
                 if r < 70:
                     for i in virus:
                         if i[-2] and draw != None:
